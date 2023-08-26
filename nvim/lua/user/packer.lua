c01cfa7a0159678e9cad97d6c5723318409b3cf8
libmartinito <lib.martinito@pm.me>
2023-08-26 18:09:55 +0800

    Added base neovim config

---
+++ nvim/lua/user/packer.lua
@@ -0,0 +1,30 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use { 'catppuccin/nvim', as = 'catppuccin' }
+	use 'nvim-lua/plenary.nvim'
+	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use('theprimeagen/harpoon')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+end)
